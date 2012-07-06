d5dc4e6494f3736bfbf0839e65a66446a9083a24
vuukle <vuukle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-06 04:24:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/free-comments-for-wordpress-vuukle/trunk@568149 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ comments.php
@@ -0,0 +1,9 @@
+<div id="comments">
+
+	<div id="respond"></div>
+
+	<div id="vuukle_div"></div>
+	<script src="http://www.vuukle.com/js/vuukle.js" type="text/javascript"></script>
+	<script type="text/javascript">create_vuukle_platform('<?php global $Vuukle; print $Vuukle->Settings['AppId']; ?>');</script>
+
+</div>
